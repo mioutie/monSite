c600a6043fed75648c85ec24b4433554192f22f1
mioutie <139978917+mioutie@users.noreply.github.com>
2023-10-16 15:05:35 +0200

    php mon site

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ma page</title>
+</head>
+<body>
+    <h1>coucou les amis!</h1>
+    
+</body>
+<?php
+//j'affichye le prénom
+echo "paul";
+echo "<br> ";
+echo "ochon";//j'affiche le nom
+#ceci est aussi un commentaire
+echo "<br> paul ochon";
+/*les commentaire vous aideront lorsque vous reviendrez dans 6mois et que vous aurez tout oublié.*/ 
+echo "<br>super";
+echo"<br>";
+
+/*si vous souhaitez voir les caracteres qui sont utilisés pour entourer les mots*/
+echo 'j\'écris à l\'encre de chine';
+/* le caractere anti-slash précéde le 2ème apostrophe qui echape ainsi à sa fonction naturelle qui est d'entourer des chaine de caracteres dans le code ici, on mat des guillemets 
+au débuts et à la fin du texte et il n'est donc pas nécessaires d'echapper l'apostrophe .Par contre il faut échapper le guillemet du prenom*/
+
+echo "<br>j'envoie des lettre à \"ma dulcinée\"";
+/*les variable 
+une variable est une conteneur d'une donnée unique, c'est à dire une boite à chaussures qui ne contient q'une seule chaussure.
+une variable contient une information temporaire qui peut étre numerique, de ype alphabétique (chaine de caractere), de type date , de type objet ou d'autres types.
+Le nom d'une variable doit commencer le caractére $ suivi d'une lettre ou un signe _(barre de 8) et ne doit jamais commencer par un chiffre.Les noms des variables sont sensibles à la casse (majuscule/miniscule).
+
+Le signe * sert donc à affecter une */ 
+
+$prenom = "téo"
+</html>
